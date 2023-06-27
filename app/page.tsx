781a043912ee9a4187de09863b3a9676abc482c6
Wasim Ullah <wasim.ullah@aol.com>
2023-06-27 21:58:22 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,14 @@ const Index = () => {
         <div className='flex h-screen '>
             <div className='m-auto text-center'>
                 <div className='inline-block'>
-                    <h1 id='name'>Matheus</h1>
-                    <h2 id='title'>Principal Engineer, <Link href="https://vercel.com">Vercel</Link></h2>
+                    <h1 id='name'>Wasim</h1>
+                    <h2 id='title'>Freelance Marketing Expert, <Link href="https://vercel.com">Vercel</Link></h2>
                 </div >
                 <div style={{ height: '1.5rem' }} />
                 <p id="social">
-                    <Link href="https://twitter.com/matheusfrndes" target="_blank">Twitter</Link> |{' '}
-                    <Link href="https://www.linkedin.com/in/matheusfrndes" target="_blank">LinkedIn</Link> |{' '}
-                    <Link href="https://instagram.com/matheusfrndes" target="_blank">Instagram</Link> |{' '}
-                    <Link href="https://github.com/matheuss/website" target="_blank">Source</Link>
+                    <Link href="https://twitter.com/wasimxullah" target="_blank">Twitter</Link> |{' '}
+                    <Link href="https://www.linkedin.com/in/wasim-ullah" target="_blank">LinkedIn</Link> |{' '}
+                    <Link href="https://cal.com/wasim" target="_blank">Cal.com</Link>
                 </p>
             </div >
             <video autoPlay loop muted src="white1.mp4"></video>
